6ea44e4b6a490c42ec7038e19c266508cfdf823d
Ghanni Idris <idrisghanni@gmail.com>
2022-06-25 22:21:02 +0100

    created the simple prompt arithmetic calculator

---
+++ script.js
@@ -0,0 +1,18 @@
+const num1 = parseFloat(prompt("Enter First number: "));
+const num2 = parseFloat(prompt("Enter Second number: "));
+const operator = prompt("Enter Operator (+, -, *, /)");
+let result = 0;
+if(isNaN(num1) || isNaN(num2)){
+    alert("Error!! Refresh")
+} else{
+    if(operator == "+"){
+        result = num1 + num2;
+    } else if(operator == "-"){
+        result = num1 - num2;
+    } else if(operator == "*"){
+        result = num1 * num2;
+    } else if(operator == "/"){
+        result = num1 / num2;
+    }
+}
+alert(num1 + operator + num2 + "=" + result)
